7cb9a2cfaccce6d274281e120a230cc82e6caecf
phpcoder2022 <phpcoder2022@yandex.ru>
2024-05-21 19:53:16 +0300

    left only dist

--- src/errors.ts
+++
@@ -1,6 +0,0 @@
-export class ParseError extends Error {
-  constructor(message: string, public data?: object) {
-    super(message + (data ? '\nexists error.data' : ''));
-    this.name = this.constructor.name;
-  }
-}
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { processFileIs } from './process-file-is';
-export { processCalls } from './process-calls';
