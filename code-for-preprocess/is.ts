b2863855af0cba571f2972e64e217ff2c01a79df
phpcoder2022 <phpcoder2022@yandex.ru>
2024-05-21 10:23:08 +0300

    Initial project settings

---
+++ code-for-preprocess/is.ts
@@ -0,0 +1,19 @@
+export function is<Wide, Narrow extends Wide>(
+  predicate: <Input>(
+    value: Wide & Input,
+    replaceCompileToRuntime: (
+      returnToCompile: Narrow,
+      returnToRuntime: Wide & Input
+    ) => typeof returnToCompile
+  ) => Narrow | typeof isnt,
+) {
+  return (value: Wide): value is Narrow => predicate(
+    value,
+    /* WEBPACK_DEL => returnToCompile */
+    // Второй аргумент используется для препроцессинга после tsc
+    // На него заменяется весь вызов функции в рантайме
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (returnToCompile, _returnToRuntime) => returnToCompile,
+  ) !== isnt;
+}
+export const isnt = Symbol('@is/isn\'t');
